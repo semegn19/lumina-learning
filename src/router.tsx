1f09b614f9a523bfe7574a7c37ef57dff44918f8
Semegn19 <semegnnyx@gmail.com>
2026-06-16 15:40:58 +0300

    feat: create router

--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,7 @@ import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient();    
 
   const router = createRouter({
     routeTree,
