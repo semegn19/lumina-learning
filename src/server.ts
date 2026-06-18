ab8c86f932e25c1b596af4841995d2d6722ab4c4
Semegn19 <semegnnyx@gmail.com>
2026-06-18 15:58:32 +0300

    feat: add error handling

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "./lib/error-capture";
 
 import { consumeLastCapturedError } from "./lib/error-capture";
-import { renderErrorPage } from "./lib/error-page"; 
+import { renderErrorPage } from "./lib/error-page";  
 
 type ServerEntry = {
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
