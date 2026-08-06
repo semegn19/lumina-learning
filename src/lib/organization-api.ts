e270c379d2fa61bad5688408a3e01cf7564b118e
Semegn19 <semegnnyx@gmail.com>
2026-08-06 18:07:48 +0300

    feat: add organization profile

--- src/lib/organization-api.ts
+++ src/lib/organization-api.ts
@@ -1,4 +1,4 @@
-import { api } from "./api-client";        
+import { api } from "./api-client";         
 import type { OrganizationSettings } from "./api-types";
 
 export async function getOrganizationSettings(): Promise<OrganizationSettings> {
