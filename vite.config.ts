cc1e06008910165d0ac7d57c930d461c37e677c0
Semegn19 <semegnnyx@gmail.com>
2026-08-20 05:37:00 +0300

    feat: prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,25 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tailwindcss(),
-    tanstackStart({
-      server: { entry: "server" },
-    }),
+
+    tanstackStart(),
+
+    nitro(),
+
     react(),
   ],
 
-  // Dev proxy — avoids CORS errors in development
-  // All requests to /api/* are forwarded to the Django server.
   server: {
     proxy: {
       "/api": {
-        target: process.env["VITE_API_URL"] ?? "http://localhost:8000",
+        target:
+          process.env["VITE_API_URL"] ?? "http://localhost:8000",
         changeOrigin: true,
         secure: false,
       },
